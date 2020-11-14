524c60fd6ef00b3c58f7b38b3ed9140f4f3bc3e9
Dennis Wethmar <denniswethmar@gmail.com>
2020-11-14 16:14:27 +0100

    Refactored TestAuthenticated

--- pkg/api/feed/handler_test.go
+++ pkg/api/feed/handler_test.go
@@ -7,13 +7,17 @@ import (
 
 	"github.com/dwethmar/atami/pkg/api/testutil"
 	"github.com/dwethmar/atami/pkg/service"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAuthenticated(t *testing.T) {
 	authService := service.NewAuthServiceMemory()
 	handler := NewHandler(authService)
 	req := httptest.NewRequest("GET", "/", nil)
-	testutil.TestWithAuthorizationHeader(t, authService, req, handler, http.StatusOK)
+	testutil.WithAuthorizationHeader(req, authService)
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+	assert.Equal(t, http.StatusOK, rr.Code, rr.Body.String())
 }
 
 func TestUnauthenticated(t *testing.T) {
@@ -21,5 +25,5 @@ func TestUnauthenticated(t *testing.T) {
 	req := httptest.NewRequest("GET", "/feed", nil)
 	rr := httptest.NewRecorder()
 	handler.ServeHTTP(rr, req)
-	testutil.TestStatus(t, req, handler, http.StatusUnauthorized)
+	assert.Equal(t, http.StatusUnauthorized, rr.Code, rr.Body.String())
 }
--- pkg/api/testutil/auth.go
+++ pkg/api/testutil/auth.go
@@ -3,30 +3,27 @@ package testutil
 import (
 	"fmt"
 	"net/http"
-	"testing"
 
 	"github.com/dwethmar/atami/pkg/api/token"
 	"github.com/dwethmar/atami/pkg/auth"
-	"github.com/stretchr/testify/assert"
 )
 
-// TestWithAuthorizationHeader test if handles authorization
-func TestWithAuthorizationHeader(t *testing.T, authService auth.Service, req *http.Request, handler http.Handler, expectedStatus int) bool {
+// WithAuthorizationHeader test if handles authorization
+func WithAuthorizationHeader(req *http.Request, authService auth.Service) error {
 	user, err := authService.Register(auth.CreateUser{
 		Username: "test",
 		Email:    "test@test.nl",
 		Password: "Test1234!@#$",
 	})
-	if !assert.NoError(t, err) {
-		return false
+	if err != nil {
+		return err
 	}
 
 	accessToken, err := token.CreateToken(user.UID, user.Username, 4100760000)
-	if !assert.NoError(t, err) {
-		return false
+	if err != nil {
+		return err
 	}
 
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", accessToken.AccessToken))
-
-	return TestStatus(t, req, handler, expectedStatus)
+	return nil
 }
