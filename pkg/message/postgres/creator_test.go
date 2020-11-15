d3287cd3bce41006b194782916fb90731e85530d
Dennis Wethmar <denniswethmar@gmail.com>
2020-11-15 17:23:31 +0100

    Added user ID to create message test in postgres

--- pkg/message/postgres/creator_test.go
+++ pkg/message/postgres/creator_test.go
@@ -12,7 +12,8 @@ import (
 func TestCreate(t *testing.T) {
 	assert.NoError(t, database.WithTestDB(t, func(db *sql.DB) error {
 		newMessage := message.NewMessage{
-			Text: "wow",
+			Text:            "wow",
+			CreatedByUserID: 1,
 		}
 		message.TestCreator(t, NewCreator(db), newMessage)
 		return nil
