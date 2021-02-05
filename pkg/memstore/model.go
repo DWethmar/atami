6f8a45cd27fbe8d961acde6ae5e90de7000381b8
Dennis Wethmar <denniswethmar@gmail.com>
2021-02-05 14:46:56 +0100

    Removed node package

--- pkg/memstore/model.go
+++ pkg/memstore/model.go
@@ -13,3 +13,12 @@ type User struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
+
+// The Message model
+type Message struct {
+	ID              int
+	UID             string
+	Text            string
+	CreatedByUserID int
+	CreatedAt       time.Time
+}
